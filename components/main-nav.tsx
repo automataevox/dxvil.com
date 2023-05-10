a1265c108fb3496cc2a8ed3ebfada5041d27a419
dxvil-exe <musicbyblakk@gmail.com>
2023-05-10 15:59:34 +0200

    for now always shown navbar links

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -20,7 +20,7 @@ export function MainNav({ items }: MainNavProps) {
         </span>
       </Link>
       {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
+        <nav className="flex gap-6">
           {items?.map(
             (item, index) =>
               item.href && (
@@ -28,7 +28,7 @@ export function MainNav({ items }: MainNavProps) {
                   key={index}
                   href={item.href}
                   className={cn(
-                    "flex items-center text-lg font-semibold text-muted-foreground sm:text-sm",
+                    "flex items-center text-sm font-semibold text-muted-foreground sm:text-sm",
                     item.disabled && "cursor-not-allowed opacity-80"
                   )}
                 >
