5325b2e125bfa47bf01254a98c5455e298fa55c0
dxvil-exe <musicbyblakk@gmail.com>
2023-05-18 15:07:34 +0200

    added toltip on my pfp

--- components/profileAvatar.tsx
+++ components/profileAvatar.tsx
@@ -1,6 +1,7 @@
 import { siteConfig } from "@/config/site"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { Skeleton } from "./ui/skeleton"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
 
 interface ProfileAvatarProps {
     isLoading:boolean
@@ -11,9 +12,19 @@ export default function ProfileAvatar({isLoading, discordData}:ProfileAvatarProp
     return(
         !isLoading ?
         <div>
-            <Avatar className="h-24 w-24">
-                <AvatarImage src={`https://cdn.discordapp.com/avatars/${siteConfig.profile?.discordUserId}/${discordData?.data?.discord_user?.avatar}`} alt="discord_avatar" />
-            </Avatar>
+             <TooltipProvider>
+                    <Tooltip >
+                        <TooltipTrigger asChild>
+                        <Avatar className="h-24 w-24">
+                            <AvatarImage src={`https://cdn.discordapp.com/avatars/${siteConfig.profile?.discordUserId}/${discordData?.data?.discord_user?.avatar}`} alt="discord_avatar" />
+                        </Avatar>
+                        </TooltipTrigger> 
+                        <TooltipContent>
+                            <p className="capitalize">From Discord</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            
         </div>
         :
         <div>
