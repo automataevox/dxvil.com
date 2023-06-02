3deadbfd925621a700740b841c36fd3b4ddd0bd3
dxvil-exe <musicbyblakk@gmail.com>
2023-06-02 13:59:21 +0200

    added index file for renderers

--- app/playground/cgen/page.tsx
+++ app/playground/cgen/page.tsx
@@ -1,39 +1,38 @@
 'use client'
+import ComponentRenderer from "@/components/componentRenderer";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { siteConfig } from "@/config/site";
 import React from "react";
 
-interface componentStructure {
-    setting: {
-        variation: {
-            name: string
-            palceholder: string
-        } | any
-    } | any
-}
-
 export default function CGen() {
     const [currentComponent, setCurrentComponent] = React.useState<any | null>(null)
-    function findComponent(component: any) {
-        return component == currentComponent
+    const [componentSettings, setComponentSettings] = React.useState<any | null>({});
+    
+    function handleComponentChange(component:any){
+        setCurrentComponent(component)
     }
-    function getComponent(): any {
-        return siteConfig.cgen.components.find(findComponent)
-    }
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+        const { name, value } = event.target;
+        setComponentSettings((prevSettings: any) => {
+          const newSettings = {...prevSettings};
+          newSettings[name] = value;
+          return newSettings;
+        });
+      }
     return (
         <section className="container grid h-[40vw] grid-flow-col gap-6 pb-8 pt-6 md:py-10">
             <h2 className="scroll-m-20 pb-2 text-2xl font-semibold first:mt-0">
                 <Card className="flex p-2">
                     <Card className="flex max-h-[40rem] w-48 flex-col overflow-auto">
                         {siteConfig.cgen.components.map((component) => {
-                            if (currentComponent == undefined) {
-                                setCurrentComponent(component)
-                            }
                             return (
-                                <Button variant={"ghost"} onClick={() => setCurrentComponent(component)}>
+                                <Button variant={"ghost"} onClick={() => handleComponentChange(component)}>
                                     {component.name}
                                 </Button>
                             )
@@ -41,20 +40,47 @@ export default function CGen() {
                     </Card>
                     <Card className="border-0">
                         <CardHeader>
-                            {currentComponent?.name}
+                            {currentComponent?.name || "Choose component"}
                         </CardHeader>
-                        <CardContent>
-                            {currentComponent?.settings ? Object.entries(currentComponent?.settings).map((setting):any => {
-                                /*setting[1]?.variation.map((value:any) =>{
-                                    //console.log(value)
-                                    return(
-                                        <Input id={value.name} key={value.name} type="text" placeholder={value.placeholder}>
-                                            
-                                        </Input>
-                                    )
-                                })*/
-                            })
-                            : <p>No input...</p>}
+                        <CardContent className={"flex flex-col gap-5"}>
+
+                                <ComponentRenderer componentName={currentComponent?.name} componentSettings={componentSettings}/>
+                            <Card className="flex flex-auto gap-5 border-0">
+                                {currentComponent?.settings ? currentComponent?.settings.map((setting: { placeholder: any; name: any; variations:any}):any => {
+                                    if(setting.variations){
+                                        return(
+                                            <Select>
+                                                <SelectTrigger className="min-w-[180px]">
+                                                <SelectValue placeholder="Select style" />
+                                                </SelectTrigger>
+                                                <SelectContent >
+                                                    <SelectGroup>
+                                                        {        
+                                                            setting?.variations?.map((value: any) => {
+                                                                return(
+                                                                    <SelectItem key={value} id={value} value={""}>{value}</SelectItem>
+                                                                )
+                                                            }) 
+                                                        }
+                                                    </SelectGroup>
+                                                </SelectContent>
+                                            </Select>
+                                        )
+                                    } else {
+                                        return (
+                                            <Input
+                                                type="text"
+                                                placeholder={setting.placeholder}
+                                                value={componentSettings[setting.name] || null}
+                                                name={setting.name}
+                                                key={setting.name}
+                                                onChange={handleInputChange}
+                                                />
+                                        )
+                                    }
+                                })
+                                : null}
+                            </Card>
                         </CardContent>
                     </Card>
                 </Card>
---
+++ components/componentRenderers/index.ts
@@ -0,0 +1,3 @@
+export { default as AccordionRenderer } from "./accordionRenderer"
+export { default as AlertDialogRenderer } from "./alertDialogRenderer"
+export { default as AvatarRenderer } from "./avatarRenderer"
